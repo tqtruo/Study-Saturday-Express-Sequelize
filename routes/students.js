2b721cc65ac9fbe579d992b5e842161b55f7c007
Theodore Truong <tqtruo@gmail.com>
2019-08-18 14:47:31 -0500

    Finished all tests

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,84 @@
 const router = require('express').Router();
+const Student = require('../db/models/student');
 
+
+router.get('/', async (req, res, next)=>{
+    try{
+        let students = await Student.findAll()
+        res.send(students);
+    }
+    catch(error){
+        next(error)
+    }
+})
+
+router.get('/:id', async (req, res, next)=>{
+    try{
+        let student = await Student.findOne({
+            where:{
+                id: req.params.id
+            }
+        })
+        if(student){
+            res.send(student)
+        }
+        else {
+            res.sendStatus(404)
+        }
+    }
+    catch(error){
+        next(error)
+    }
+})
+
+router.post('/', async (req, res, next)=>{
+    try{
+        let newStudent = await Student.create(req.body)
+        if(newStudent){
+            res.status(201).send(newStudent)
+        }
+    }
+    catch(error){
+        next(error)
+    }
+})
+
+router.put('/:id', async (req, res, next)=>{
+    try{
+    let updatedStudent = await Student.update(req.body, {
+        where:{
+            id: req.params.id
+        },
+        returning: true,
+        plain: true
+    })
+    //await console.log(updatedStudent)
+    res.send(updatedStudent[1])
+    }
+    catch(error){
+        next(error)
+    }
+})
+
+router.delete('/:id', async (req, res, next)=>{
+    try{
+    let student = Student.findOne({
+        where:{
+            id: req.params.id
+        }
+    })
+    if(student){
+        Student.destroy({
+            where:{
+                id: req.params.id
+            }
+        })
+        res.status(204).send(Student)
+    }
+}
+    catch(error){
+        next(error)
+    }
+})
 
 module.exports = router;
--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,62 @@
 const router = require('express').Router();
+const Test = require('../db/models/test')
+const Student = require('../db/models/student')
 
+router.get('/', async (req,res,next)=>{
+    try{
+        let tests = await Test.findAll({
+    })
+        if(tests){
+            res.send(tests)
+        }
 
+    }
+    catch(error){
+        next(error)
+    }
+})
+
+router.get('/:id', async (req,res,next)=>{
+    try{
+        let test = await Test.findOne({
+            where:{
+                id: req.params.id
+            }
+        })
+        if(test){
+            res.send(test)
+        }
+    }
+    catch(error){
+        next(error)
+    }
+})
+
+router.post('/student/:studentId', async(req,res,next)=>{
+    try{
+    let student = await Student.findOne({
+        where:{
+            id: req.params.studentId
+        }
+    })
+    let newTest = await Test.create(req.body)
+
+    if(student){
+        await newTest.setStudent(student)
+        res.status(201).send(newTest)
+    }
+}
+    catch(error){
+        next(error)
+    }
+})
+
+router.delete('/:id', async (req,res,next)=>{
+    await Test.destroy({
+        where:{
+            id:req.params.id
+        }
+    })
+    res.sendStatus(204)
+})
 module.exports = router;
